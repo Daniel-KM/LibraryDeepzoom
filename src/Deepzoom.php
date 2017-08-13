4b2a1187b0b98d46c3b292cf565e0e8b94d6df19
Daniel Berthereau <Daniel.github@Berthereau.net>
2017-08-14 00:00:00 +0200

    Updated a link.

--- src/Deepzoom.php
+++ src/Deepzoom.php
@@ -654,7 +654,7 @@ class Deepzoom
      *
      * Expects arguments to be properly escaped.
      *
-     * @see Omeka\Service\Cli
+     * @see Omeka\Stdlib\Cli
      *
      * @param string $command An executable command
      * @return string|false The command's standard output or false on error
